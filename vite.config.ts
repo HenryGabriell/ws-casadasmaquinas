ca1d8548f5130ccc0337bf967c0664c4d54b986d
Henry Gabriel <96191361+HenryGabriell@users.noreply.github.com>
2025-10-22 21:20:15 -0300

    tentando arrumar

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: "/ws-casadasmaquinas/",
+  base: "/ws-casadasmaquinas",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
